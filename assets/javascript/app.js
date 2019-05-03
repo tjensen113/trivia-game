a2bd119f38e8197ab4d9001c3f18972739510a48
Thomas Jensen <thomasjensen@Thomass-MacBook-Pro.local>
2019-05-03 18:49:16 -0500

    fixed typos

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,13 @@
+function onSubmit(){
+   var score=0;
+   var numOfQuestions=5;
+   var ansArr=['c','c','d','c','d'];
 
+
+   var q1 = document.forms[quiz][q1].value;
+   var q2 = document.forms[quiz][q2].value;
+   var q3 = document.forms[quiz][q3].value;
+   var q4 = document.forms[quiz][q4].value;
+   var q5 = document.forms[quiz][q5].value;
+    alert(q5)
+}
